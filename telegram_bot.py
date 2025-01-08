4c45e55b8693744a7bea64b9fa4125b63006cb21
Romigo97 <roma.kolyada939@gmail.com>
2025-01-08 16:53:28 +0300

    automatic publication of all photos from a directory in telegram with an interval

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,15 +1,33 @@
 import telegram
 import os
 from dotenv import load_dotenv
+import random 
+import time
+import argparse
 
 
 load_dotenv()
 BOT = telegram.Bot(token=os.environ['TG_TOKEN'])
 CHANNEL_ID = os.environ['TG_CHANNEL_ID']
 
+def publish_photos(directory, interval):
+    images = os.listdir(directory)
+    
+    while True:
+        random.shuffle(images)
 
-def main():
-    BOT.send_photo(chat_id=CHANNEL_ID, photo=open('images/nasa_apod6.jpg', 'rb'))
+        for image in images:
+            image_path = os.path.join(directory, image)
+            with open(image_path, 'rb') as image:
+                BOT.send_photo(chat_id=CHANNEL_ID, photo=image)
+                time.sleep(interval * 3600)        
+
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Скрипт для автоматической публикации в Telegram-канал.')
+    parser.add_argument('directory', type=str, help='Путь к директории с изображениями')
+    parser.add_argument('--interval', type=int, default=os.getenv('INTERVAL', 4), help='Интервал между публикациями в часах')
+    
+    args = parser.parse_args()
+    publish_photos(args.directory, args.interval)
+    
